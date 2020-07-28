e2fcc6a808453b9eb871f5e7168e0c9c1605ab8f
Rohan Goyal <goyal.rohan.03@gmail.com>
2020-07-28 14:32:25 +0400

    Cleaned docstrings, added methods to generate flow-control regexes

--- notepad-convert.el
+++ notepad-convert.el
@@ -8,7 +8,7 @@
 (defun write (obj file)
   (write-region (format "%S" obj) nil file))
 
-(defvar file "/opt/GMAT/R2018a/extras/notepad++.xml")
+(defvar file "./notepad++.xml")
 (defvar xmltree (xml-parse-file file))
 (write xmltree "./notepad-xml.el")
 
@@ -21,11 +21,13 @@
 
 (setq valid-nodes ())
 (defun find-nodes (node)
-  "Walk an xml tree as parsed, to find the node whose attribute returns true for the predicate test. Eg: Find a node such that it's attribute name contains the string 'Keywords1' would be (find-node tree 'name' (lambda (str) ))"
-  ;; Tree-head refers to the root node of the xml rep.
+  "Walk an xml tree as parsed, to find the node who's name matches Keywords or Words"
   (when (listp node)
     (cond
-     ((or ((cl-search "Keywords" (xml-get-attribute node 'name))) (cl-search "Words" (xml-get-attribute node 'name)))
+     (
+      (or
+       (cl-search "Keywords" (xml-get-attribute node 'name))
+       (cl-search "Words" (xml-get-attribute node 'name)))
       (push node valid-nodes))
      )
     (mapc 'find-nodes (xml-node-children node))))
@@ -34,11 +36,12 @@
   (cl-search "Keywords" (xml-get-attribute node 'name)))
 
 (find-nodes (car xmltree))
-(write-region (format "%S" valid-nodes) nil "./validnodes.el")
+(write valid-nodes "./validnodes.el")
 
 
 (defun table-from-nodes (_nodelist) ;In this case, we expect nodelist to be the valid-nodes we defined earlier
-  (setq nodelist (reverse _nodelist))
+  "Returns a hashtable where the keys are \"Keyword1\",etc. and the vals are lists of words"
+  (setq nodelist (reverse _nodelist)) ;
   (setq adict (make-hash-table :test 'equal))
   (setq nodelist-clean (seq-filter (lambda (y) (> (length y) 2)) nodelist)) ;Remove the "empty" sets of words
   (dolist (kw nodelist-clean)
@@ -47,26 +50,36 @@
   adict
   )
 
-(defvar conversion-table) ;;Converts from name Keywords1 to objects, Keywords2 to functions, etc. Manually defined
+(defvar conversion-table) ;;Converts from name Keywords1 to objects, Keywords2 to functions, etc. Manually defined, TODO
 
 (setq table (table-from-nodes valid-nodes))
 (write table "./table.el")
 
 
-"Plan: Find every node whose name attr contains \"Keyword\". Shove them all in a hashtable where the name (keywords1, etc.) is the key and the val is a list of words. After that, have a hashtable which maps it so that keywords1 is funcs, 2 is objs, etc. (or other way round).
-DONE-ish
+(defun regexes-from-hash (hash)
+  (defvar regexes ())
+  (maphash (lambda (k v)
+             (push (regexp-opt v 'words) regexes)
+             )
+           table
+           )
+  regexes
+  )
+(setq regexes (regexes-from-hash table))
+(setq keys (hash-table-keys table))
+
+(write regexes "regex.el") ; Generates and saves regexes. Now, we use these regexes, the conversion table, and the keys list, to set the font-lock pairs
 
 
+"
 Nums, operators, delimiters can be fairly standard (maybe just inherit from prog-mode). Ignore indentation since that's tricky
 
 We let the user define a mode and syntable to inherit (default to one of the C-modes)
 
 For auto-complete, we use https://justinhj.github.io/2018/10/24/radix-trees-dash-and-company-mode.html
 So once we have the hash, we basically iterate over it to get all the keywords and store them in a list. Then we use seq-reduce magic to pass them to a radixtree. We write this radixtree to a file.
-
-Key problem is to generate a hash from xml. That needs solving.
 "
 
 
-"
-Generate hashtable/assoc_list from XML:"
+
+
